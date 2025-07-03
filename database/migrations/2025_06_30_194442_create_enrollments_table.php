0843e3821273db0837c1861715c2ee929afe673b
Mancilla <lm15029@ues.edu.sv>
2025-07-03 09:40:07 -0600

    feat: add migration and models for tenants, teachers, students, courses, and enrollments

---
+++ database/migrations/2025_06_30_194442_create_enrollments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->increments('enrollment_id');
+
+            $table->integer('tenant_id');
+            $table->integer('student_id');
+            $table->integer('course_id');
+
+            // curdate() es una función de MySQL que devuelve la fecha actual
+            $table->string('enrollment_date')->default(DB::raw('curdate()'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
